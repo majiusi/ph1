9f5d3bb2d51696a86ea8d052735d3ba21761bc4c
chxyao <bluote@gmail.com>
2016-11-26 17:37:40 +0900

    Add Login function

---
+++ IOS/Signinout/Signinout/LoginAfterUITabBarViewController.m
@@ -0,0 +1,40 @@
+//
+//  LoginAfterViewController.m
+//  Signinout
+//
+//  Created by yaochenxu on 2016/11/26.
+//  Copyright © 2016年 maiasoft. All rights reserved.
+//
+
+#import "LoginAfterUITabBarViewController.h"
+
+@interface LoginAfterUITabBarViewController ()
+
+@end
+
+@implementation LoginAfterUITabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void) viewWillAppear:(BOOL)animated{
+    self.navigationController.navigationBar.hidden = YES;
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ IOS/Signinout/Signinout/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Signinout
+//
+//  Created by yaochenxu on 2016/11/26.
+//  Copyright © 2016年 maiasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
