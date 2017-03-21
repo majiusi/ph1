b949e06cdddf04b335455bee9d10897d0ee80a62
chxyao <bluote@gmail.com>
2017-03-21 22:04:32 +0900

    add password change page

---
+++ IOS/Signinout/Signinout/PasswordChangeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PasswordChangeTableViewController.h
+//  Signinout
+//
+//  Created by yaochenxu on 2017/3/21.
+//  Copyright © 2017年 maiasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PasswordChangeTableViewController : UITableViewController
+
+@end
---
+++ IOS/Signinout/Signinout/SetupTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetupTableViewController.h
+//  Signinout
+//
+//  Created by yaochenxu on 2017/3/20.
+//  Copyright © 2017年 maiasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetupTableViewController : UITableViewController
+
+@end
