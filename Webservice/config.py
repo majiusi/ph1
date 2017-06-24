1429cee7bd4621703115eccd74973f101fec9dd8
bluote <bluote@gmail.com>
2017-06-24 18:47:44 +0900

    nothing

--- Webservice/config.py
+++ Webservice/config.py
@@ -5,6 +5,7 @@ conf_file = os.path.split(os.path.realpath(__file__))[0] + '/logging.conf'
 logging.config.fileConfig(conf_file)
 
 SECRET_KEY = 'the key of maiasoft 2016-2017.'
+#SQLALCHEMY_DATABASE_URI = 'mysql://dbuser:Admin001@54.199.240.10/maiaDB'
 SQLALCHEMY_DATABASE_URI = 'mysql://dbuser:Admin001@localhost/maiaDB'
 SQLALCHEMY_POOL_SIZE = 100
 SQLALCHEMY_POOL_RECYCLE = 3600
