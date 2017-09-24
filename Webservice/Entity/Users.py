d0ab6864a09147c3c35895b20bec7935aaae96d6
bluote <bluote@gmail.com>
2017-09-24 20:31:03 +0900

    change db structure

--- Webservice/Entity/Users.py
+++ Webservice/Entity/Users.py
@@ -26,14 +26,14 @@ class User(db.Model):
 
     enterprise_id = db.Column(db.String(10), primary_key=True, nullable=False)
     employee_id = db.Column(db.String(10), primary_key=True)
-    name = db.Column(db.String(20), nullable=False, unique=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
     pwd = db.Column(db.String(256), nullable=False)
     auth_id = db.Column(db.String(10), nullable=False)
     last_login_at = db.Column(db.DateTime, nullable=False)
     valid = db.Column(db.Integer, nullable=False, server_default=db.text("'1'"))
     create_by = db.Column(db.String(10), nullable=False)
     create_at = db.Column(db.DateTime, nullable=False, server_default=db.text("CURRENT_TIMESTAMP"))
-    update_by = db.Column(db.String(10), nullable=False)
+    update_by = db.Column(db.String(100), nullable=False)
     update_at = db.Column(db.DateTime, nullable=False, server_default=db.text("CURRENT_TIMESTAMP"))
     update_cnt = db.Column(db.Integer, nullable=False, server_default=db.text("'1'"))
 
